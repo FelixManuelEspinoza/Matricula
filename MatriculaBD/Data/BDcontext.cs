d925cc4294e18db62eb83e8af07eed1882723326
FelixManuelEspinoza <81267662+FelixManuelEspinoza@users.noreply.github.com>
2022-08-09 01:28:25 -0300

    Agregar archivos de proyecto.

---
+++ MatriculaBD/Data/BDcontext.cs
@@ -0,0 +1,24 @@
+﻿using MatriculaBD.Data.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatriculaBD.Data
+{
+    public class BDcontext : DbContext
+    {
+        public DbSet<Medico> Medicos { get; set; }
+
+        public BDcontext()
+        {
+
+        }
+        public BDcontext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ MatriculaBD/Data/Entidades/Medico.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatriculaBD.Data.Entidades
+{
+    [Index(nameof(MatriculaMedica), Name = "Numero de matricula", IsUnique =true)]
+    
+    public class Medico
+    {
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public string DNI { get; set; }
+
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public string MatriculaMedica { get; set; }
+
+    }
+}
